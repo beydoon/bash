8df5a693cc45f4f59ba04c5b14556e755407fdc6
Khurram Subhani <khurramsubhani@gmail.com>
2020-06-09 19:49:05 -0400

    minor update added header

--- sed/ere_example_01/example01.sh
+++ sed/ere_example_01/example01.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+# example01.sh
+# ERE sed example01
+# version 1.0.0
+# Bedouin Technologies (c) Copyright 2020 *GPL*
+# 20200609
+#set -x
 
 basedir="$(pwd)"
 file1="$basedir/file1.log"
